606a442e93fdde81cc00bb9fa21c87cc91967457
Thiago Corbalan <thiago.corbalan@elo7.com>
2023-09-09 22:49:39 -0300

    Add layer auth with JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
+
+const JWT_SECRET = process.env.SECRET;
 
 @Module({
 	controllers: [AuthController],
 	providers: [AuthService],
-	imports: [UsersModule],
+	imports: [
+		UsersModule,
+		JwtModule.register({
+			global: true,
+			secret: JWT_SECRET,
+			signOptions: { expiresIn: '60s' },
+		}),
+	],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,29 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from './../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-	constructor(private usersService: UsersService) {}
+	constructor(
+		private usersService: UsersService,
+		private jwtService: JwtService,
+	) {}
 
 	async signIn(username: string, password: string): Promise<any> {
 		const user = await this.usersService.getUser(username, password);
 
-		if (user.password !== password) {
+		if (user?.password !== password) {
 			throw new UnauthorizedException();
 		}
 
-		return user;
+		const payload = {
+			id: user.id,
+			name: user.name,
+			email: user.email,
+		};
+
+		return {
+			access_token: await this.jwtService.signAsync(payload),
+		};
 	}
 }
