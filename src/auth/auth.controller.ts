9cd7ee11ad65a8a11a2b460f93a4ed68dee83a15
Thiago Corbalan <thiago.corbalan@elo7.com>
2023-09-09 23:43:59 -0300

    Add Auth Guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,16 @@
 import { UsersDto } from 'src/users/users.dto';
 import { AuthService } from './auth.service';
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Post,
+	Request,
+	UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +20,11 @@ export class AuthController {
 	@Post('login')
 	signIn(@Body() signInDto: UsersDto) {
 		return this.authService.signIn(signInDto.email, signInDto.password);
+	}
+
+	@UseGuards(AuthGuard)
+	@Get('profile')
+	getProfile(@Request() req) {
+		return req.user;
 	}
 }
